ed5a7ba79e0a72d97c80cda908c0c58448c3aae8
Jegan Kumar <56663008+Jegankumard@users.noreply.github.com>
2024-01-21 00:38:55 +0530

    Update jenkins1.sh

--- jenkins/jenkins1.sh
+++ jenkins/jenkins1.sh
@@ -1,9 +1,7 @@
 sudo apt-get update
 
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
-    /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-    https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
     /etc/apt/sources.list.d/jenkins.list > /dev/null
 
 sudo apt update
@@ -13,3 +11,5 @@ sudo apt install openjdk-17-jre -y
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
 sudo systemctl status jenkins
+
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
