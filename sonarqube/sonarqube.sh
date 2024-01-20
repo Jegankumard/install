51231da080f834a241d4da887fb93c3ccf3391e0
Jegan Kumar <56663008+Jegankumard@users.noreply.github.com>
2024-01-21 00:42:47 +0530

    Update sonarqube.sh

--- sonarqube/sonarqube.sh
+++ sonarqube/sonarqube.sh
@@ -5,6 +5,7 @@ sudo apt-get install docker.io -y
 sudo usermod -aG docker $USER   #my case is ubuntu
 newgrp docker
 sudo chmod 777 /var/run/docker.sock
+sudo docker ps
 # run sonarqube image
 docker run -d --name sonar -p 9000:9000 sonarqube:lts-community
 # access using <ec2-public-ip:9000>
