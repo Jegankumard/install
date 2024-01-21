c5a2516f20b0a341bf5c1b79a7a3a1b55154c6b7
Jegan Kumar <56663008+Jegankumard@users.noreply.github.com>
2024-01-21 13:31:10 +0530

    Update jenkins.sh

--- jenkins/jenkins.sh
+++ jenkins/jenkins.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Install java
 sudo apt update
-sudo apt install fontconfig openjdk-17-jre
+sudo apt install fontconfig openjdk-17-jre -y
 java -version
 #Install jenkins
 sudo wget -O /usr/share/keyrings/jenkins-keyring.asc https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
